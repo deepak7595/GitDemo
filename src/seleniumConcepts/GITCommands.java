a2e10acb41345fc431e53d76a696e0e0cd21aa7d
Deepak <johndeepak444@gmail.com>
2025-02-14 22:06:56 +0530

    first message

---
+++ src/seleniumConcepts/GITCommands.java
@@ -0,0 +1,29 @@
+package seleniumConcepts;
+
+public class GITCommands {
+
+	public static void main(String[] args) {
+		
+		// Basic git commands
+		//https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html
+
+//		or create a new repository on the command line
+//		echo "# GitDemo" >> README.md
+//		git init
+//		git add README.md
+//		git commit -m "first commit"
+//		git branch -M main
+//		git remote add origin https://github.com/deepak7595/GitDemo.git
+//		git push -u origin main
+//		
+//		
+//		
+//		or push an existing repository from the command line
+//		git remote add origin https://github.com/deepak7595/GitDemo.git
+//		git branch -M main
+//		git push -u origin main
+		
+		
+	}
+
+}
